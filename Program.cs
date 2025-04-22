ebccfe3bac2f111d7a844b57d81a2ca9a9f384e3
name_u <name_u@VICTOR22>
2025-04-22 09:36:58 +0300

    2. Другий етап лабораторної роботи. Зміни у головному файлі Program.cs. Доданий простір імен, що відповідає проекту. Додана точка входу. Проведений тест. Все працює коректно.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra_lab1_2_git_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+}
